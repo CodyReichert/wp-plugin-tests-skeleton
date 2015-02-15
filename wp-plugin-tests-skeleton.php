ef9a93241c3572fdd933cf844bc5213b16ff40f9
Cody Reichert <cody@reichertbrothers.com>
2015-02-15 17:09:12 -0600

    initial commit

---
+++ wp-plugin-tests-skeleton.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ Plugin Name: WP Plugin Tests Skeleton
+ Description: A starting point for integrating PHPUnit tests with the Wordpress testing suite and Travis-CI.
+ Author: Cody Reichert
+ Version: 0.1.0
+ License: GNU General Public License v3 or later
+*/
+
